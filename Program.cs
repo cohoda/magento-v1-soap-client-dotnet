d252d9b32f78867c728d829f05ddc3f8feb436a4
Alex Brown <alex@cohoda.com>
2019-10-14 22:54:14 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace magento_dotnet_wrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
